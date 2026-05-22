9252d43070e1d69e38aaec0d0edccfd08b0d0a32
tai hopkins <hopkins02@allegheny.edu>
2026-05-22 08:34:17 -0400

    updating skills slider imgs

--- src/components/SkillsSlider.jsx
+++ src/components/SkillsSlider.jsx
@@ -1,24 +1,25 @@
 import React from "react";
 
 const topSkills = [
-  { img: "/img/python-html.png", alt: "Python logo", skill: "Python", value: 70 },
+  { img: "/img/python.png", alt: "Python logo", skill: "Python", value: 70 },
   { img: "/img/html5.png", alt: "HTML logo", skill: "HTML", value: 50 },
   { img: "/img/css.png", alt: "CSS logo", skill: "CSS", value: 75 },
   { img: "/img/sqlite.png", alt: "SQLite logo", skill: "SQLite", value: 72 },
   { img: "/img/r.png", alt: "R logo", skill: "R", value: 60 },
-  { img: "/img/cs.jpg", alt: "C logo", skill: "C", value: 45 },
+  { img: "/img/javascript.png", alt: "JavaScript logo", skill: "JavaScript", value: 60 },
+  { img: "/img/c-sharp-logo.jpg", alt: "C# logo", skill: "C#", value: 45 },
 ];
 
 const bottomSkills = [
-  { img: "/img/cat.png", alt: "placeholder", skill: "p5Js", value: 75 },
-  { img: "/img/cat.png", alt: "placeholder", skill: "Adobe Photoshop", value: 50 },
-  { img: "/img/cat.png", alt: "placeholder", skill: "Adobe Illustrator", value: 50 },
-  { img: "/img/cat.png", alt: "placeholder", skill: "React", value: 25 },
-  { img: "/img/cat.png", alt: "placeholder", skill: "Skill 11", value: 50 },
-  { img: "/img/cat.png", alt: "placeholder", skill: "Skill 12", value: 50 },
-  { img: "/img/cat.png", alt: "placeholder", skill: "Skill 13", value: 50 },
-  { img: "/img/cat.png", alt: "placeholder", skill: "Skill 14", value: 50 },
+  { img: "/img/p5dotjs.png", alt: "p5.js logo", skill: "p5.js", value: 75 },
+  { img: "/img/photoshop-.png", alt: "Adobe Photoshop logo", skill: "Adobe Photoshop", value: 50 },
+  { img: "/img/Illustrator-.png", alt: "Adobe Illustrator logo", skill: "Adobe Illustrator", value: 50 },
+  { img: "/img/react.png", alt: "React logo", skill: "React", value: 25 },
+  { img: "/img/mongodb.png", alt: "MongoDB logo", skill: "MongoDB", value: 55 },
+  { img: "/img/docker.png", alt: "Docker logo", skill: "Docker", value: 45 },
+  { img: "/img/streamlit.png", alt: "Streamlit logo", skill: "Streamlit", value: 50 },
 ];
+
 
 function SkillCard({ img, alt, skill, value }) {
   return (
